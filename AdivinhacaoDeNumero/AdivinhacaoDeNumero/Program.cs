d3a0a598236793d09abb0487ce6c3c67f9799607
GustavoCruzE <144917374+GustavoCruzE@users.noreply.github.com>
2024-09-25 17:33:16 -0300

    Jogo de adivinhação com número aleatório

---
+++ AdivinhacaoDeNumero/AdivinhacaoDeNumero/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Buffers;
+
+Random random = new Random();
+int minimo = 1;
+int maximo = 100;
+int numAleatorio;
+int resposta;
+int numRespostas;
+String novamente = "";
+bool jogarNovamente = true;
+
+while (jogarNovamente)
+{
+    numAleatorio = random.Next(minimo, maximo + 1);
+    numRespostas = 0;
+    resposta = 0;
+
+    while (resposta != numAleatorio)
+    {
+        Console.Write("Digite um número entre " + minimo + " - " + maximo + ": ");
+        resposta = Convert.ToInt32(Console.ReadLine());
+
+        if (resposta < numAleatorio)
+        {
+            Console.WriteLine(resposta + " é um valor muito baixo");
+        }
+        else if (resposta > numAleatorio)
+        {
+            Console.WriteLine(resposta + " é um valor muito alto");
+        }
+        numRespostas++;
+    }
+    Console.WriteLine("Número gerado:" + numAleatorio);
+    Console.WriteLine("Resposta: " + resposta);
+    Console.WriteLine("Tentativas: " + numRespostas);
+    Console.WriteLine("Parabéns, você acertou!");
+    Console.Write("Deseja jogar novamente? (S/N): ");
+    novamente = Console.ReadLine();
+
+    novamente = novamente.ToUpperInvariant();
+
+    if (novamente == "S")
+    {
+        jogarNovamente = true;
+    }
+    else if (novamente == "N")
+    {
+        jogarNovamente = false;
+    }
+}
+
